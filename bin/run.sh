6197eff23900ae10582eb74332dd8b84eb932bde
alex <bigbadmath@gmail.com>
2020-12-13 20:21:04 -0500

    run.sh waits and traps for easier cleanup

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# just a simple run script to start the servers and allow cleanup
+# https://spin.atomicobject.com/2017/08/24/start-stop-bash-background-process/
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
 
 # "processing steps" on 4000 and 4001
 python3 processors/upper.py &
@@ -9,3 +14,5 @@ python3 switchboard.py &
 
 # "client" on 3000
 python3 operator.py &
+
+wait
